7553894b9af5540105314fa77d5d6813d52b2e9c
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-19 14:57:27 -1000

    set up finally complete 1st whack

--- server.js
+++ server.js
@@ -1,9 +1,47 @@
 const net = require('net');
-
-const PORT = process.env.PORT || 8080; 
+const element = require('./elements')
+const PORT = process.env.PORT || 8080;
 
 const server = net.createServer((socketInstance) => {
-  console.log('client Connected');
+  //previous request here
+  //heres where all code goes
+  process.stdout.write('A client is connected \n');
+  socketInstance.setEncoding('UTF8');
+  console.log('1st thing', socketInstance)
+
+  socketInstance.on('data', (data) => {
+    console.log('what is', data); // = GET / HTTP /1.1
+    console.log('!!!!!!!!!!!!!!!!!', element) // = 404,hydrogen etc..
+    let requestURI = data.split('', 2)[1]; //Takes out the resource from the long request string
+    console.log('??????????', requestURI)
+    let index = requestURI.indexOf('/'); // = 0;
+    console.log('|||||||||||', index)
+    let url = requestURI.slice(index); //Removes whatevers was before '/'
+    console.log('::::::::::: ',requestURI);
+    console.log('>>>>><<<<<<<<<<: ', url);
+
+    //proccess request and response here ^^^
+    //concat head and body and send that to the client.
+    let response = '';
+
+    response = response.concat(`http/1.1 200 OK\n`);
+    response = response.concat(`content type: text/html\n`);
+    response = response.concat(`\n`);
+    response = response.concat(element.hydrogen);
+    console.log(response);
+
+    socketInstance.write(response);
+
+    socketInstance.end(); //final step
+  });
+
+  socketInstance.on('end', () => {
+    console.log('client disconnected')
+  });
+
+
+
+
 });
 
 server.listen(PORT, () => {
