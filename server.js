952a83293668f8ecb1a3ce83098cf9b2916019b2
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-19 19:48:00 -1000

    oh my goodness

--- server.js
+++ server.js
@@ -2,55 +2,67 @@ const net = require('net');
 const element = require('./elements')
 const PORT = process.env.PORT || 8080;
 
-const server = net.createServer((socketInstance) => {
-  //previous request here
-  //heres where all code goes
 
-  let savedData = "";
-  console.log("YESSSAHHHHHH", savedData);
-  // console.log('1st thing', socketInstance)
+const statusMessage = {
+  good: `200 OK`,
+  notFound: `404 Not Found`,
+  forbidden: `403 Forbidden`,
+  serverError: `500 Internal Server Error`
+}
+
+const server = net.createServer((socketInstance) => {
+
   socketInstance.setEncoding('UTF8');
 
-  socketInstance.on('data', (chunk) => {
-    console.log('what is', chunk); // = GET / HTTP /1.1...
 
-    // console.log('!!!!!!!!!!!!!!!!!', element) // = 404,hydrogen etc..
-    let requestURI = chunk.split(" ", 3); //Takes out the resource from the long request string
-    console.log('??????????', chunk.split(" ", 3));
+  socketInstance.on('data', (chunk, sender) => {
+    console.log('what is', chunk); 
+  
+        let request = chunk.split("\r\n")[0].split(' ');
+        let requestMethod = request[0];
+        let requestURI = request[1];
+        let requestHTTPVersion = request[2];
+        console.log('powpowpow', request);
+        console.log('popopopopopop', requestMethod)
+        console.log('llllllllll', requestURI)
+        console.log('uuuuuuuuuuu', requestHTTPVersion)
+        // console.log('keyyyy', chunk);
+  
+        if (requestMethod === 'GET' || requestURI === '/') 
+         socketInstance.write(createrHeader(requestURI, requestHTTPVersion, statusMessage.good, element.index)) 
 
-    let index = requestURI.join(" "); // = 1;
-    console.log('|||||||||||', index)
-
-    server.listen(8080, () => {
-      console.log('port 8080' + '\n');
-
-      // function getInformations(element, sender) {
-      //   let request = element.split("\r\n");
-      //   let method = request[0].split(' ');
-      //   let wanted = method[1];
-      //   let versio = method[2];
-      //   console.log('powpowpow', request);
-             
-      // }
-      console.log('nononononoon', getInformations())
-    })
-
-    socketInstance.end( () => {
-
-    }); 
-  });
-
+          //  chunk.write(createHeader(chunk, wanted2, statusMessage.good, element.index));
+         
+        // } else if (wanted === '/helium.html') {
+        //   sender.write(createHeader(sender, wanted2, statusMessage.good, element.helium));
+        // } else if (wanted === '/hydrogen.html') {
+        //   sender.write(createHeader(sender, wanted2, statusMessage.good, element.hydrogen));
+        // } else if (wanted === '/css/styles.css') {
+        //    sender.write(createHeader(sender, wanted2, statusMessage.good, element.css));
+        // } else {
+        //    sender.write(createHeader(sender, wanted2, statusMessage.notFound, element.fourohfour));
+        // }
+        // sender.destroy();
+    
+    
+  
+    function createHeader(socketInstance, version, status, source) {
+      return `${version} ${status}
+    status: ${version} ${status}
+    server:${process.env.USER} ${process.env.TERM_PROGRAM} ${process.env.TERM_PROGRAM_VERSION}
+    date: ${new Date()}
+    ${source}`
+    
+    }
   socketInstance.on('end', () => {
     console.log('client disconnected')
+  })
 
-  });
-
-
-  server.on('end', () => {
-    console.log('connection ended');
-  });
-});
-
+  // server.on('end', () => {
+  //   console.log('connection ended');
+  // });
+})
+}) 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-});
+})
